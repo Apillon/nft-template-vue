897dcb155fffcd22354002a00935375b97f89374
Voronella1 <voronella@gmail.com>
2024-11-07 23:24:37 +0600

    Update production.ts

--- config/production.ts
+++ config/production.ts
@@ -1,8 +1,8 @@
 // Moonbase
-const config: ConfigInterface = {
-  CHAIN_ID: '0x507',
-  CONTRACT_ADDRESS: '0x2f2DD020a5f1636D5ED1fB09a627a975E191A978',
-};
+//const config: ConfigInterface = {
+ // CHAIN_ID: '0x507',
+ // CONTRACT_ADDRESS: '0x2f2DD020a5f1636D5ED1fB09a627a975E191A978',
+//};
 // Moonbeam
 // const config: ConfigInterface = {
 //   CHAIN_ID: '0x504',
@@ -11,7 +11,7 @@ const config: ConfigInterface = {
 // Astar
  const config: ConfigInterface = {
    CHAIN_ID: '0x250',
-  CONTRACT_ADDRESS: '',
+  CONTRACT_ADDRESS: ' '0x2f2DD020a5f1636D5ED1fB09a627a975E191A9',
  };
 
 export default config;
